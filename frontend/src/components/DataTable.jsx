b7a76b84e7c5fd727d4f910283415d6cb056d9bf
garhus2020 <61534454+garhus2020@users.noreply.github.com>
2022-05-13 23:51:37 +0300

    Add files via upload

--- frontend/src/components/DataTable.jsx
+++ frontend/src/components/DataTable.jsx
@@ -6,20 +6,26 @@ function DataTable({data}) {
     <Container>
        <Paper sx={{paddingBlock:"30px"}}>
          <div>
-          <Typography variant='h3' sx={{marginBottom:"30px"}} color="error" className='title'>{data.reversed_params===1? "Simpson's Paradox DETECTED":"Simpson's Paradox Not DETECTED" }</Typography>
+          <Typography variant='h3' sx={{marginBottom:"30px"}}  className='title'>
+          Simpson's Paradox <Typography variant='h3' component={"span"}  color="error">{data.reversed_params===0? "Not DETECTED":" DETECTED" }</Typography></Typography>
           <Typography variant='h3' className='title'>Confounding variable: <Typography variant='h3' component={"span"} color={"error"}>{data.confounding_variable}</Typography></Typography>
       
-          <Typography variant='h5' className='title' sx={{marginBlock:"30px"}}>The proportion of reversed associations in subgroups: <Typography variant='h4' component={"span"} color={"error"}> {data.reversed_params&&data.reversed_params*100+"%"}</Typography></Typography>
-          {
-            data.response&&
-            <img src={data.response} width='100%' height='800' alt="" />
-          }
+          <Typography variant='h5' className='title' component={"div"} sx={{marginTop:"20px",marginBottom:"50px"}}>
+          The Subgroups which exhibit reversal: <Typography variant='h5' component={"h5"} color={"error"}>
+               {data.revs.map((item,index)=>(
+                 <span key={index}>{index!==0? ',':''}"{item}"</span>
+               ))}</Typography></Typography>
+         
         {
           data.agg_data&&
          <div>
               <div>
               <table aria-label="custom pagination table">
-              <caption><Typography variant='h4' sx={{marginBottom:"20px"}}>Aggregated Data</Typography></caption>
+              <caption>
+                <Typography variant='h4' sx={{marginBottom:"20px"}}>Aggregated Data</Typography>
+                <Typography variant='p' component={"h5"} sx={{marginBottom:"20px",color:"gray"}}>Description: Aggregated Data representing 
+                average "{Object.keys(data.agg_data[0])[1]}" rate/amount for each "{Object.keys(data.agg_data[0])[0]}"</Typography>
+                </caption>
             <thead>
               <tr>
              { Object.keys(data.agg_data[0]).map((heading,b)=>(
@@ -33,7 +39,8 @@ function DataTable({data}) {
                 <tr key={index}>
                   {Object.values(row).map((item,b)=>(
                     <td key={b} style={{ width: 160 }} align="center">
-                 {item}
+                      
+                      {Object.keys(data.agg_data[0]).length===b+1?   Number(item).toFixed(4):item}
                   </td>
                   ))}
                 </tr>
@@ -44,7 +51,12 @@ function DataTable({data}) {
     
               <div>
               <table aria-label="custom pagination table">
-              <caption><Typography variant='h4' sx={{marginBottom:"20px"}}>Disaggregated Data</Typography></caption>
+              <caption><Typography variant='h4' sx={{marginBottom:"20px"}}>Disaggregated Data</Typography>
+              <Typography variant='p' component={"h5"} sx={{marginBottom:"20px",color:"gray"}}>Description: Disaggregated Data representing 
+
+              average "{Object.keys(data.disagg_date[0])[2]}" rate/amount for each "{Object.keys(data.disagg_date[0])[0]}" 
+              conditioning by "{Object.keys(data.disagg_date[0])[1]}"</Typography>
+              </caption>
             <thead>
               <tr>
               { Object.keys(data.disagg_date[0]).map((heading,b)=>(
@@ -57,7 +69,7 @@ function DataTable({data}) {
                 <tr key={index}>
                   {Object.values(row).map((item,b)=>(
                     <td key={b} style={{ width: 160 }} align="center">
-                 {item}
+                 {Object.keys(data.disagg_date[0]).length===b+1?   Number(item).toFixed(4):item}
                   </td>
                   ))}
                 </tr>
@@ -68,7 +80,12 @@ function DataTable({data}) {
     
               <div>
               <table aria-label="custom pagination table">
-              <caption><Typography variant='h4' sx={{marginBottom:"20px"}}>Aggregated Data with adjustments</Typography></caption>
+              <caption><Typography variant='h4' sx={{marginBottom:"20px"}}>Aggregated Data with adjustments</Typography>
+              <Typography variant='p' component={"h5"} sx={{marginBottom:"20px",color:"gray"}}>Description:
+               Adjusted Aggregated Data representing average "{Object.keys(data.fixed_agg_data[0])[1]}" rate/amount for each
+                 "{Object.keys(data.fixed_agg_data[0])[0]}" .
+               IPW method is used to balance the data distribution illustrated below during the aggregation.</Typography>
+              </caption>
             <thead>
               <tr>
               { Object.keys(data.fixed_agg_data[0]).map((heading,b)=>(
@@ -82,7 +99,7 @@ function DataTable({data}) {
                 <tr key={index}>
                   {Object.values(row).map((item,b)=>(
                     <td style={{ width: 160 }} key={b} align="center">
-                 {item}
+                 {Object.keys(data.fixed_agg_data[0]).length===b+1?   Number(item).toFixed(4):item}
                   </td>
                   ))}
                 </tr>
@@ -93,7 +110,21 @@ function DataTable({data}) {
          </div>
 }
          </div>
-        
+         {
+            data.dist&&
+            <div>
+              <Typography variant='h4' sx={{marginBottom:"20px"}}>Data Distribution </Typography>
+               <img src={data.dist} width='100%' height='400' alt="" />
+            </div>
+          }
+         {
+            data.response&&
+          <div>
+              <Typography variant='h4' className='Graphic'>Graphical Representation </Typography>
+            <img src={data.response} width='100%' className='Graphic-img' height='800' alt="" />
+          </div>
+          }
+          
      </Paper>
     </Container>
   )
